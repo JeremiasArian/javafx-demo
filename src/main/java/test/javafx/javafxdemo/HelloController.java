fb4f2f5c10921231c727f6601f0ba00016c5febb
jere <159722626+JeremiasArian@users.noreply.github.com>
2024-03-13 21:38:35 -0300

    Add exit button

--- src/main/java/test/javafx/javafxdemo/HelloController.java
+++ src/main/java/test/javafx/javafxdemo/HelloController.java
@@ -1,14 +1,13 @@
 package test.javafx.javafxdemo;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onExitButtonClick() {
+        Platform.exit();
     }
 }
